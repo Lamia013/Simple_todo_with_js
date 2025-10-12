6484461cb78b1bf2baf11e3c7406f9bda5c6e9e9
Lamia Jamal <lamia.jamal.cs@gmail.com>
2025-10-13 01:48:21 +0600

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('#button').disabled = true;
+
+    document.querySelector('#task').onkeyup = () => {
+        document.querySelector('#button').disabled = false;
+
+        if(document.querySelector('#task').value.length >0) {
+            document.querySelector('#button').disabled = false;
+        }
+        else {
+            document.querySelector('#button').disabled = true;
+        }
+    }
+
+    document.querySelector('form').onsubmit = () => {
+
+        let task = document.querySelector('#task').value;
+        let li = document.createElement('li');
+
+        li.innerHTML = task;
+
+        document.querySelector('#tasks').append(li);
+
+        document.querySelector('#task').value = '';
+
+        return false;
+    }
+});
