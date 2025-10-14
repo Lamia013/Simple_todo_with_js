65e6d74f6603657de8ae885e0b034de32cbf98a4
Lamia013 <lamiaj2002@gmail.com>
2025-10-14 21:06:58 +0600

    added color option

--- todo.js
+++ todo.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded",() => {
 
-    let checklist = true;
+    let selectedColor = null;
 
-    if(checklist)
-    {
-        document.querySelector('h2').innerHTML = "Empty list";
-    }
+    document.querySelector('h2').innerHTML = "Empty list";
     document.querySelector('#button').disabled = true;
+    document.querySelectorAll('button').forEach((button) => {
+        button.disabled = true;
+    })
 
     document.querySelector('#task').onkeyup = () => {
-        document.querySelector('#button').disabled = false;
 
-        if(document.querySelector('#task').value.length >0)
-        {
-            document.querySelector('#button').disabled = false;
-        }
-        else
-        {
-            document.querySelector('#button').disabled = true;
-        }
+        const hasText = document.querySelector('#task').value.length > 0;
+
+        document.querySelector('#button').disabled = !hasText;
+        document.querySelectorAll('button').forEach((button) => {
+            if(button.id !== 'button') 
+            {
+                button.disabled = !hasText;
+            }
+        });
     }
 
     document.querySelector('form').onsubmit = () => {
@@ -26,12 +26,32 @@ document.addEventListener("DOMContentLoaded",() => {
         let li = document.createElement('li');
 
         li.innerHTML = task.toUpperCase();
+        if(selectedColor)
+        {
+            li.style.color = selectedColor;
+        }
 
         document.querySelector('#list').append(li);
         document.querySelector('#task').value = '';
 
         document.querySelector('#button').disabled = true;
         document.querySelector('h2').innerHTML = "";
+        document.querySelectorAll('button').forEach((button) => {
+            if(button.id !== 'button') 
+            {
+                button.disabled = true;
+                selectedColor = null;
+            }
+        });
         return false;
     }
+    document.querySelectorAll('button').forEach((button) => {
+        if(button.id !== 'button') 
+        {  
+            button.onclick = () => {
+                selectedColor = button.dataset.color; 
+                document.querySelector('#task').focus();
+            };
+        }
+    });
 });
